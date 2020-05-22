8b7aff8749552a37a0d788e2e8f08dff8f1a438d
Tilman Bartelsmeyer <tilman.bartelsmeyer@yale.edu>
2020-05-22 00:58:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -20,6 +20,44 @@ def filler
 end
 
 =begin
+
+Home Team:
+
+* team name: Brooklyn Nets
+* colors: Black, White
+* players:
+
+|          Stat          | Info | Info |  Info | Info | Info   |
+|:------------------:|:-------------:|:------------:|:------------:|:-------------:|:-------------:|
+| **Player Name**    |  Alan Anderson| Reggie Evans | Brook Lopez  | Mason Plumlee | Jason Terry   |
+| **Number**         | 0             | 30           | 11           | 1             | 31            |
+| **Shoe**           | 16            | 14           | 17           | 19            | 15            |
+| **Points**         | 22            | 12           | 17           | 26            | 19            |
+| **Rebounds**       | 12            | 12           | 19           | 11            | 2             |
+| **Assists**        | 12            | 12           | 10           | 6             | 2             |
+| **Steals**         | 3             | 12           | 3            | 3             | 4             |
+| **Blocks**         | 1             | 12           | 1            | 8             | 11            |
+| **Slam Dunks**     | 1             | 7            | 15           | 5             | 1             |
+
+Away Team:
+
+* team name: Charlotte Hornets
+* colors: Turquoise, Purple
+* players:
+
+|        Stat       |     Info          |         Info     |              Info |         Info     |         Info      |
+|:------------------:|:-----------------:|:-----------------:|:-----------------:|:---------------:|:-----------------:|
+| **Player Name**  | Jeff Adrien     | Bismack Biyombo    | DeSagna Diop      | Ben Gordon      | Kemba Walker   |
+| **Number**         | 4                 | 0                 | 2                 | 8               | 33                |
+| **Shoe**           | 18                | 16                | 14                | 15              | 15                |
+| **Points**         | 10                | 12                | 24                | 33              | 6                 |
+| **Rebounds**       | 1                 | 4                 | 12                | 3               | 12                |
+| **Assists**        | 1                 | 7                 | 12                | 2               | 12                |
+| **Steals**         | 2                 | 22                 | 4                 | 1               | 7                |
+| **Blocks**         | 7                 | 15                | 5                 | 1               | 5                 |
+| **Slam Dunks**     | 2                 | 10                | 5                 | 0               | 12                |
+
+
 top_hash[team][players][arr_index][player_name] = string
 k1_team (home,away) > v1_team hash
   k2_cat (team_name, colors, players)
