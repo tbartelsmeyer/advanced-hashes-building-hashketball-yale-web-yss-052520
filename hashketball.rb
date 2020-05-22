7af9939bc765003276be2e7bb8eb6556f9c56fef
Tilman Bartelsmeyer <tilman.bartelsmeyer@yale.edu>
2020-05-22 00:35:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,37 @@
-# Write your code here!
+def game_hash
+  
+end
+
+=begin
+top_hash[team][players][arr_index][player_name] = string
+k1_team (home,away) > v1_team hash
+  k2_cat (team_name, colors, players)
+  v2_catval
+    team_name > string 
+    colors > array
+      strings
+    players > array
+      hashes > string or int
+        :player_name
+        :number
+        :shoe
+        :points
+        :rebounds
+        :assists
+        :steals
+        :blocks
+        :slam_dunks
+    v2_category
+=end
+
+players => [
+  {:player_name => "Brook Lopez"}
+  :number
+  :shoe
+  :points
+  :rebounds
+  :assists
+  :steals
+  :blocks
+  :slam_dunks
+  ]
